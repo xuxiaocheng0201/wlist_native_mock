eb863ba9e931a11ef55f848af5aa8d94880294c5
xuxiaocheng <2762267080@qq.com>
2024-09-01 11:59:47 +0800

    feat(lib): 实现core/server

---
+++ src/core/helper.rs
@@ -0,0 +1,137 @@
+pub(crate) mod utils {
+    use std::sync::Arc;
+
+    use dashmap::DashMap;
+    use rand::Rng;
+    use tokio::task::{spawn_blocking, JoinError};
+
+    pub fn generate_string(length: usize) -> String {
+        const ALL: &str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let mut key = Vec::with_capacity(length);
+        let mut rand = rand::thread_rng();
+        for _ in 0..length {
+            key.push(ALL.as_bytes()[rand.gen_range(0..ALL.len())]);
+        }
+        unsafe { String::from_utf8_unchecked(key) }
+    }
+
+    pub async fn random_id_and_put<V: Send + Sync + 'static>(map: &'static DashMap<Arc<String>, V>, value: V) -> Result<Arc<String>, JoinError> {
+        spawn_blocking(move || random_id_and_put_sync(map, value)).await
+    }
+
+    pub fn random_id_and_put_sync<V>(map: &DashMap<Arc<String>, V>, value: V) -> Arc<String> {
+        loop {
+            let id = Arc::new(generate_string(32));
+            let entry = map.entry(id);
+            if let dashmap::Entry::Occupied(_) = &entry {
+                continue;
+            }
+            break Arc::clone(entry.insert(value).key())
+        }
+    }
+}
+
+pub mod hasher {
+    use std::sync::Arc;
+
+    use bytes::Bytes;
+    use faster_hex::{hex_encode, Error};
+    use md5::Md5;
+    use sha2::Sha256;
+    use tokio::sync::Mutex;
+    use tokio::task::spawn_blocking;
+
+    /// MD5
+    pub(crate) fn hex32(src: &[u8]) -> Result<String, Error> {
+        let mut buffer = vec![0; 32];
+        hex_encode(src, &mut buffer)?;
+        Ok(unsafe { String::from_utf8_unchecked(buffer) })
+    }
+
+    /// SHA256
+    pub(crate) fn hex64(src: &[u8]) -> Result<String, Error> {
+        let mut buffer = vec![0; 64];
+        hex_encode(src, &mut buffer)?;
+        Ok(unsafe { String::from_utf8_unchecked(buffer) })
+    }
+
+    macro_rules! define_hasher {
+        ($vis: vis $name: ident($hasher: ty) $digest: ident -> $hex: ident ? $h: literal) => {
+            $vis struct $name {
+                inner: Arc<Mutex<$hasher>>,
+            }
+
+            impl $name {
+                $vis fn new() -> Self {
+                    Self { inner: Arc::new(Mutex::new($digest::Digest::new())), }
+                }
+
+                $vis async fn update(&self, data: &Bytes) {
+                    let mut inner = self.inner.clone().lock_owned().await;
+                    let data = data.clone();
+                    spawn_blocking(move || {
+                        let inner: &mut $hasher = &mut *inner;
+                        $digest::Digest::update(inner, data.as_ref());
+                    }).await.expect(concat!("update ", $h, " should not fail"))
+                }
+
+                $vis async fn finalize(self) -> String {
+                    let hasher = Arc::try_unwrap(self.inner).expect(concat!("waiting ", $h, " update finish")).into_inner();
+                    spawn_blocking(move || $hex(&$digest::Digest::finalize(hasher)).expect(concat!($h, " should be valid hex"))).await
+                        .expect(concat!("finalize ", $h, " should not fail"))
+                }
+            }
+        };
+    }
+
+    define_hasher!(pub Md5Hasher(Md5) md5 -> hex32 ? "md5");
+    define_hasher!(pub Sha256Hasher(Sha256) sha2 -> hex64 ? "sha256");
+}
+
+pub mod buffer {
+    use bytes::{BufMut, Bytes};
+    use bytes::buf::UninitSlice;
+
+    #[inline]
+    pub unsafe fn new_read_buffer(ptr: *const u8, cap: usize) -> Bytes {
+        debug_assert!(!ptr.is_null(), "ptr is null");
+        Vec::from_raw_parts(ptr as _, cap, cap).into()
+    }
+
+    pub struct WriteBuffer {
+        ptr: *mut u8,
+        len: usize,
+        cap: usize,
+    }
+
+    impl WriteBuffer {
+        #[inline]
+        pub unsafe fn new(ptr: *mut u8, cap: usize) -> Self {
+            debug_assert!(!ptr.is_null(), "ptr is null");
+            Self { ptr, len: 0, cap, }
+        }
+    }
+
+    unsafe impl BufMut for WriteBuffer {
+        #[inline]
+        fn remaining_mut(&self) -> usize {
+            self.cap - self.len
+        }
+
+        #[inline]
+        unsafe fn advance_mut(&mut self, cnt: usize) {
+            let remaining = self.remaining_mut();
+            if remaining < cnt {
+                panic!("advance_mut: cnt({cnt}) > remaining_mut({remaining}).");
+            }
+            self.len += cnt;
+        }
+
+        #[inline]
+        fn chunk_mut(&mut self) -> &mut UninitSlice {
+            unsafe { UninitSlice::from_raw_parts_mut(self.ptr.add(self.len), self.remaining_mut()) }
+        }
+    }
+
+    unsafe impl Send for WriteBuffer { }
+}
---
+++ src/core/server.rs
@@ -0,0 +1,44 @@
+use std::net::SocketAddr;
+
+use anyhow::Result;
+use tokio::net::ToSocketAddrs;
+
+#[derive(Debug)]
+pub struct WlistServer {
+    _private_construct: ()
+}
+
+impl WlistServer {
+    pub fn local_addr(&self) -> SocketAddr {
+        unimplemented!("WlistServer is not implemented in mock.")
+    }
+
+    pub async fn start(_addr: impl ToSocketAddrs) -> Result<Self> {
+        unimplemented!("WlistServer is not implemented in mock.")
+    }
+
+    pub async fn stop(self) -> Result<()> {
+        unimplemented!("WlistServer is not implemented in mock.")
+    }
+}
+
+pub mod users {
+    use anyhow::Result;
+    use once_cell::sync::Lazy;
+    use tokio::sync::RwLock;
+
+    use crate::core::helper::utils::generate_string;
+
+    static PASSWORD: Lazy<RwLock<String>> = Lazy::new(|| RwLock::new(generate_string(8)));
+
+    pub async fn login(username: &str, password: &str) -> Result<bool> {
+        if username != "admin" { return Ok(false); }
+        Ok(*PASSWORD.read().await == password)
+    }
+
+    pub async fn reset_admin_password() -> Result<String> {
+        let password = generate_string(8);
+        *PASSWORD.write().await = password.clone();
+        Ok(password)
+    }
+}
