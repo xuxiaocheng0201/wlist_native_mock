a77162883ca20a6a7b45706880e10571dd4db54b
xuxiaocheng <2762267080@qq.com>
2024-09-02 17:03:46 +0800

    fix(lib): 修正构建错误

--- wlist_native/src/common/database.rs
+++ wlist_native/src/common/database.rs
@@ -1,22 +1,18 @@
-use std::path::PathBuf;
-
 use anyhow::Result;
 use rusqlite::Connection;
 
-use crate::common::workspace::{get_cache_directory, WorkspaceError};
-
-fn get_dict_path() -> Result<PathBuf, WorkspaceError> {
-    Ok(get_cache_directory()?.join("simple"))
-}
+// fn get_dict_path() -> Result<PathBuf, WorkspaceError> {
+//     Ok(get_cache_directory()?.join("simple"))
+// }
 
 pub fn initialize() -> Result<()> {
-    libsimple::release_dict(&get_dict_path()?)?;
-    libsimple::enable_auto_extension()?;
+    // libsimple::release_dict(&get_dict_path()?)?;
+    // libsimple::enable_auto_extension()?;
     rusqlite_regex::enable_auto_extension()?;
     Ok(())
 }
 
-pub fn load_extension(connection: &Connection) -> rusqlite::Result<()> {
-    libsimple::set_dict(connection, &get_dict_path().unwrap())?;
+pub fn load_extension(_connection: &Connection) -> rusqlite::Result<()> {
+    // libsimple::set_dict(connection, &get_dict_path().unwrap())?;
     Ok(())
 }
--- wlist_native/src/web/version.rs
+++ wlist_native/src/web/version.rs
@@ -1,8 +1,5 @@
 use serde::Deserialize;
 
-use crate::common::exceptions::TokenExpiredError;
-use crate::web::LOGIN_STATUS;
-
 #[derive(Debug, Deserialize, Copy, Clone, Eq, PartialEq)]
 pub enum VersionState {
     Latest,
@@ -11,6 +8,5 @@ pub enum VersionState {
 }
 
 pub async fn check_version() -> anyhow::Result<VersionState> {
-    LOGIN_STATUS.read().await.as_ref().ok_or(TokenExpiredError)?;
     Ok(VersionState::Latest)
 }
