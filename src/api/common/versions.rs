9836b7e995699f72919f39d97be0405b3a48c7ef
xuxiaocheng <2762267080@qq.com>
2025-01-11 02:59:10 +0800

    feat(api): 重命名version系列接口

--- src/api/common/versions.rs
+++ src/api/common/versions.rs
@@ -1,18 +1,18 @@
-#[flutter_rust_bridge::frb(sync, getter, name = "common_version")]
+#[flutter_rust_bridge::frb(sync, getter, name = "getCommonVersion")]
 /// The version of the common API.
 /// Only displayed in detail version information.
 pub fn get_common_api_version() -> String {
     wlist_native::common::versions::get_common_api_version()
 }
 
-#[flutter_rust_bridge::frb(sync, getter, name = "web_version")]
+#[flutter_rust_bridge::frb(sync, getter, name = "getWebVersion")]
 /// The version of the web API.
 /// Only displayed in detail version information.
 pub fn get_web_api_version() -> String {
     wlist_native::common::versions::get_web_api_version()
 }
 
-#[flutter_rust_bridge::frb(sync, getter, name = "core_version")]
+#[flutter_rust_bridge::frb(sync, getter, name = "getCoreVersion")]
 /// The version of the native API.
 /// Only displayed in detail version information.
 pub fn get_core_api_version() -> String {
